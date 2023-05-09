7ca6273441b657e5dc17e84f408299c9df3910ed
RealFatCat <trashmessage@yandex.ru>
2023-05-09 20:18:01 +0300

    init

---
+++ generate.go
@@ -0,0 +1,3 @@
+package sdk
+
+//go:generate openapi-generator generate -i ../../SpaceTradersAPI/api-docs/reference/SpaceTraders.json -o . -g go -p packageName=sdk --git-user-id realfatcat --git-repo-id go-space-traders -p enumClassPrefix=true
