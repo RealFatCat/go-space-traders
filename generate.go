a09cf66625b73bbdc1931cbfb9fe5a57ab356562
RealFatCat <trashmessage@yandex.ru>
2023-05-09 20:44:12 +0300

    add git submodule; generate will use submodule

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package sdk
 
-//go:generate openapi-generator generate -i ../../SpaceTradersAPI/api-docs/reference/SpaceTraders.json -o . -g go -p packageName=sdk --git-user-id realfatcat --git-repo-id go-space-traders -p enumClassPrefix=true
+//go:generate openapi-generator generate -i api-docs/reference/SpaceTraders.json -o . -g go -p packageName=sdk --git-user-id realfatcat --git-repo-id go-space-traders -p enumClassPrefix=true
